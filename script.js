52fdcec9e3137ae0cc839c31f769d49c5aa6dd62
Matt Oake <matt@Matts-MacBook-Air.local>
2023-01-04 16:07:56 -0400

    create array for days of week in order sun-sat. Use .getDay function to retrieve a number from 0-6 from user input unix timestamp to place this number as the slice we want from the array of days. Output to usser day of week as spelt day instead of number.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let unixStorage = 0;
+let dayOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 let unixInput = document.getElementById("unixTimeStamp");
 let theDayIs = document.getElementById("day");
 let hourMinSec = document.getElementById("hourMinSec");
@@ -9,6 +18,9 @@ function calculateTime() {
   let hrs = date.getHours();
   let mins = date.getMinutes();
   let secs = date.getSeconds();
+  let day = date.getDay();
   let formattedTime = hrs + ":" + mins + ":" + secs;
   hourMinSec.innerHTML = "THE TIME IS: " + formattedTime;
+
+  theDayIs.innerHTML = "THE DAY IS: " + dayOfWeek[day];
 }
