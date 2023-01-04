68bbc2a5a0e565ffe755024c3351b67cf1d94dea
Matt Oake <matt@Matts-MacBook-Air.local>
2023-01-04 15:59:18 -0400

    Create variable to store input from user. Fucntion stores information first and then uses this to get date and time and displays HR:M:S to user.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let inputStorage = 0;
-
+let unixStorage = 0;
 let unixInput = document.getElementById("unixTimeStamp");
+let theDayIs = document.getElementById("day");
 let hourMinSec = document.getElementById("hourMinSec");
 
 function calculateTime() {
-  inputStorage = unixInput.value;
-  hourMinSec.innerHTML = "THE TIME IS: " + inputStorage;
+  unixStorage = unixInput.value;
+  let date = new Date(unixStorage * 1000);
+  let hrs = date.getHours();
+  let mins = date.getMinutes();
+  let secs = date.getSeconds();
+  let formattedTime = hrs + ":" + mins + ":" + secs;
+  hourMinSec.innerHTML = "THE TIME IS: " + formattedTime;
 }
