076bc3802bc6be825fc84605a7854966861874be
Matt Oake <matt@Matts-MacBook-Air.local>
2023-01-04 15:24:22 -0400

    Test function

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+let inputStorage = 0;
+
 let unixInput = document.getElementById("unixTimeStamp");
 let hourMinSec = document.getElementById("hourMinSec");
+
+function calculateTime() {
+  inputStorage = unixInput.value;
+  hourMinSec.innerHTML = "THE TIME IS: " + inputStorage;
+}
+console.log("dog");
