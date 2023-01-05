94f68c55dbe8c7a66f43e8ba35123ffb86226892
Matt Oake <matt@Matts-MacBook-Air.local>
2023-01-05 15:04:06 -0400

    Build function that takes 3 numbers. With these three numbers add them together and divide them by 2 to store as a variable which will be needed for another equation to solve for the area of a triangle. use function to get teh square route of an additional equation (Herons). If the variable area that is stored is equal to 0 than return a string. Return value of variable AREA.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// #1 Write a function that will take a unix timestamp value and display a date and time (Atlantic Timezone) in the following format:example: 1658433143The day is: Thursday.The Time is: 4:52pm
+
 let unixStorage = 0;
 let dayOfWeek = [
   "Sunday",
@@ -24,3 +26,24 @@ function calculateTime() {
 
   theDayIs.innerHTML = "THE DAY IS: " + dayOfWeek[day];
 }
+
+// #2 Write a function that takes the lengths of 3 sides of a triangle and returns the area. Round answer to 2 decimal places. If area is 0 then return "Impossible Triangle".
+
+// Area = Square root of s(s - a)(s - b)(s - c)
+// s is the semi perimeter, or (a + b + c)/2.```
+
+//           example: side1 = 2, side2 = 3, side3 = 4
+//           answer: 2.90
+//           example: side1 = 1, side2 = 2, side3 = 3
+//           answer: Impossible Triangle
+
+function areaOfTriangle(a, b, c) {
+  let s = (a + b + c) / 2;
+  let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  if (area === 0) {
+    return "IMPOSSIBLE TRIANGLE";
+  }
+  return area;
+}
+
+console.log(areaOfTriangle(1, 2, 3));
