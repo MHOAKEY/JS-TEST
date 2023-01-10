e2b42eeeae68debdf73fc4f87c7a9d5a5b5b828d
Matt Oake <matt@Matts-MacBook-Air.local>
2023-01-10 15:42:23 -0400

    Move variable from inside for loop to above for loop inside funxtion. Add if statement inside for loop to check for when the character in the string given is ther letter z so that the function will know in this instance to change z into a instead of the charcode after z.

--- script.js
+++ script.js
@@ -174,11 +174,14 @@ function classAverages(objArray) {
 function changeLetter(str) {
   let newStr = str.toLowerCase();
   let result = "";
+  let change;
   for (let i = 0; i < newStr.length; i++) {
-    let change = String.fromCharCode(newStr.charCodeAt(i) + 1);
+    if (newStr.charCodeAt(i) + 1 > 122) {
+      change = String.fromCharCode(97);
+    } else change = String.fromCharCode(newStr.charCodeAt(i) + 1);
     result += change;
   }
   return result;
 }
 
-console.log(changeLetter("ABC"));
+console.log(changeLetter("ABCXYZ"));
