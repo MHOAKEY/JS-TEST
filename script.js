91cd295483127672ca3c5b1acdd55fb1d4beef72
Matt Oake <matt@Matts-MacBook-Air.local>
2023-01-12 15:49:36 -0400

    define function. create object array with names and values of canadian currency.

--- script.js
+++ script.js
@@ -257,3 +257,19 @@ function findLongWord(str) {
 //
 //    example: 42.79
 //    answer: "2 $20 bills, 1 Twoonie, 3 quarters, 1 nickel, 4 pennies"
+
+function howMuchChange(price) {
+  let arr = [
+    { name: "pennie", value: 0.01 },
+    { name: "nickel", value: 0.05 },
+    { name: "dime", value: 0.1 },
+    { name: "quarter", value: 0.25 },
+    { name: "loonie", value: 1 },
+    { name: "twonie", value: 2 },
+    { name: "$5", value: 5 },
+    { name: "$10", value: 10 },
+    { name: "$20", value: 20 },
+    { name: "$50", value: 50 },
+    { name: "$100", value: 100 },
+  ];
+}
